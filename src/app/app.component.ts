af80bfde905167e805b8144fffa92db8c32802dd
Anand Thanki <anandthanki@Anands-MBP.home>
2020-04-05 01:35:57 -0700

    Added the password generate functionality and Styled the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = "";
+
+  onChangeLength(event: any) {
+    var value = parseInt(event.target.value);
+
+    if(!isNaN(value)) {
+      this.length = value;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   onButtonClick() {
-    console.log('Button clicked!!');
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = "!@#$%^&*()";
+    let validChars = '';
+
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+
+    if(this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if(this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for(let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  shouldDisable() {
+    return !(this.length && 
+      (this.includeLetters || 
+        this.includeNumbers || 
+        this.includeSymbols));
   }
 }
